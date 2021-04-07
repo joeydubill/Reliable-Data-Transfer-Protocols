ebcd31d29385855f78cbf19375abf0177ba8d8fc
joeydubill <32132614+joeydubill@users.noreply.github.com>
2021-04-06 20:46:57 -0400

    Update abt.c

--- jwdubill/src/abt.c
+++ jwdubill/src/abt.c
@@ -15,12 +15,19 @@
 
 /********* STUDENTS WRITE THE NEXT SIX ROUTINES *********/
 
-struct Sender {
-   int seq;
-   bool wait5;
-   bool wait ack;
- 
-};
+
+// sender info
+int sender_seq;
+// 0 if waiting on ACK, 1 if waiting on layer 5
+int wait_5;
+// incremenet value for timer
+float sender_inc;
+
+// receiver info
+int rec_seq;
+
+// packet for timeouts
+struct pkt timeout_pkt;
 
 int calc_checksum(struct pkt *packet){
   int sum = 0;
@@ -36,21 +43,52 @@ int calc_checksum(struct pkt *packet){
 void A_output(message)
   struct msg message;
 {
- if (
- 
-
+ if (wait_5 != 1){
+     return;
+ }
+ struct pkt packet;
+ memcpy(packet.payload, message.data, 20);
+ packet.checksum = calc_checksum(&packet);
+ timeout_pkt = packet;
+ wait_5 = 0;
+ tolayer3(0, packet);
+ starttimer(0, sender_inc);
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(packet)
   struct pkt packet;
 {
-
+ //first check for checksum, state, and ack #
+ if (packet.checksum != calc_checksum(&packet)){
+     return;
+ }
+ if (wait_5 != 0){
+    return;
+ }
+ if (packet.acknum != sender_seq){
+  return;
+ }
+ //we made it through
+ stoptimer(0);
+ if (sender_seq == 0){
+    sender_seq = 1;
+ }else if (sender_seq == 1){
+    sender_seq = 0;
+ }else{
+    printf("impossible");
+ }
+ wait_5 = 1;
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
+ if (wait_5 != 0){
+    return;
+ }
+ tolayer3(0, timeout_pkt);
+ starttimer(0, sender_inc);
 
 }  
 
@@ -58,7 +96,10 @@ void A_timerinterrupt()
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
 {
-
+ wait_5 = 1;
+ sender_seq = 0;
+ //change??
+ sender_inc = 10;
 }
 
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
@@ -67,6 +108,33 @@ void A_init()
 void B_input(packet)
   struct pkt packet;
 {
+  struct pkt pack;
+ //check checksum and seq
+ if (packet.checksum != calc_checksum(&packet)){
+     pack.acknum = 1 - rec_seq
+      //do we need the packet data?
+     pack.checksum = calc_checksum(&packet);
+     tolayer3(1, pack);
+     return;    
+ }
+ if (packet.seqnum != rec_seq) {
+     pack.acknum = 1 - rec_seq
+     pack.checksum = calc_checksum(&packet);
+     tolayer3(1, pack);
+     return;
+ }
+ //send to 5 
+ pack.acknum = 1 - rec_seq
+ pack.checksum = calc_checksum(&packet);
+ tolayer3(1, pack);
+ tolayer5(1, packet.payload);
+ if (rec_seq == 0){
+    rec_seq = 1;
+ }else if (sender_seq == 1){
+    rec_seq = 0;
+ }else{
+    printf("impossible");
+ }
 
 }
 
@@ -74,5 +142,5 @@ void B_input(packet)
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
 {
-
+ rec_seq = 0;
 }
