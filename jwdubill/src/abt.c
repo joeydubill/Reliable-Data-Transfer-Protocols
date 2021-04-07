9f58324d1e7b952abeb5c6351af843a363bc05c0
joeydubill <32132614+joeydubill@users.noreply.github.com>
2021-04-07 14:23:25 -0400

    Update abt.c

--- jwdubill/src/abt.c
+++ jwdubill/src/abt.c
@@ -173,9 +173,9 @@ void B_input(packet)
  memcpy(pack.payload, packet.payload, 20);
  pack.checksum = calc_checksum(&packet);
  tolayer3(1, pack);
- for (int i=0; i<20; i++){
-    printf(&packet.payload[i]);
- }
+
+ printf(&packet.payload);
+ 
  //printf(packet.payload);
  tolayer5(1, packet.payload);
  if (rec_seq == 0){
