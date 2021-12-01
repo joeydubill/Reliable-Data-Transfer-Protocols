f98009aa42b9f06d50a6143e8619545a4a6e3d59
joeydubill <32132614+joeydubill@users.noreply.github.com>
2021-12-01 15:52:54 -0500

    Update abt.c

--- jwdubill/src/abt.c
+++ jwdubill/src/abt.c
@@ -1,7 +1,6 @@
 #include "../include/simulator.h"
 #include <string.h>
 #include <stdio.h>
-
 /* ******************************************************************
  ALTERNATING BIT AND GO-BACK-N NETWORK EMULATOR: VERSION 1.1  J.F.Kurose
    This code should be used for PA2, unidirectional data transfer 
@@ -13,27 +12,20 @@
    - packets will be delivered in the order in which they were sent
      (although some can be lost).
 **********************************************************************/
-
 /********* STUDENTS WRITE THE NEXT SIX ROUTINES *********/
-
-
 // sender info
 int sender_seq;
 // 0 if waiting on ACK, 1 if waiting on layer 5
 int wait_5;
 // incremenet value for timer
 float sender_inc;
-
 // receiver info
 int rec_seq;
-
 // packet for timeouts
 struct pkt timeout_pkt;
 struct msg buffer[1000];
 int bufferwriteindex;
 int bufferreadindex;
-
-
 int calc_checksum(struct pkt *packet){
   int sum = 0;
   sum += packet->seqnum;
@@ -43,20 +35,19 @@ int calc_checksum(struct pkt *packet){
   }
   return sum;
 }
-
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(message)
   struct msg message;
 {
  if (wait_5 != 1){
-     //printf("A_Output: waiting on ACK, buffering message: %s", message.data);
+     printf("A_Output: waiting on ACK, buffering message: %s", message.data);
      struct msg buff;
      memcpy(buff.data, message.data, 20);
      buffer[bufferwriteindex] = buff;
      bufferwriteindex++;
      return;
  }
-// printf("A_Output: waiting on Layer 5, sending message: %s", message.data);
+ printf("A_Output: waiting on Layer 5, sending message: %s", message.data);
  struct pkt packet;
  memcpy(packet.payload, message.data, 20);
  packet.seqnum = sender_seq;
@@ -65,7 +56,6 @@ void A_output(message)
  wait_5 = 0;
  tolayer3(0, packet);
  starttimer(0, sender_inc);
-
 }
                                                      
 /* called from layer 3, when a packet arrives for layer 4 */
@@ -75,21 +65,21 @@ void A_input(packet)
  //first check for checksum, state, and ack #
  
  if (wait_5 != 0){
-   // printf("A_Input: not waiting for ACK");
+    printf("A_Input: not waiting for ACK");
     return;
  }
  if (packet.checksum != calc_checksum(&packet)){
-     //printf("A_Input: checksum error");
+     printf("A_Input: checksum error");
      
      
      return;
  }
  if (packet.acknum != sender_seq){
-    //printf("A_Input: acknum not equal to sender_seq");
+    printf("A_Input: acknum not equal to sender_seq");
     return;
  }
  //we made it through
- //printf("A_Input: receiving ACK");
+ printf("A_Input: receiving ACK");
  stoptimer(0);
  if (sender_seq == 0){
     sender_seq = 1;
@@ -100,7 +90,7 @@ void A_input(packet)
  }
  wait_5 = 1;
  if (bufferreadindex < bufferwriteindex){
-  // printf("A_Input: sending buffered message: %s", buffer[bufferreadindex]);
+   printf("A_Input: sending buffered message: %s", buffer[bufferreadindex]);
    struct msg call = buffer[bufferreadindex];
    A_output(call);
    bufferreadindex++;
@@ -109,9 +99,7 @@ void A_input(packet)
    bufferwriteindex = 0;
  }
  
-
 }
-
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
@@ -121,11 +109,9 @@ void A_timerinterrupt()
  }
  starttimer(0, sender_inc);
  //timeout_pkt.acknum = 1 - timeout_pkt.acknum;
- //printf("A Timer interrupt, resending timeout_packet: %s", timeout_pkt.payload);
+ printf("A Timer interrupt, resending timeout_packet: %s", timeout_pkt.payload);
  tolayer3(0, timeout_pkt);
-
 }  
-
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
@@ -137,9 +123,7 @@ void A_init()
  bufferwriteindex = 0;
  bufferreadindex = 0;
 }
-
 /* Note that with simplex transfer from a-to-B, there is no B_output() */
-
 /* called from layer 3, when a packet arrives for layer 4 at B*/
 void B_input(packet)
   struct pkt packet;
@@ -167,7 +151,7 @@ void B_input(packet)
      return;
  }
  //send to 5 
-// printf("B_input: sending ack and to layer 5: %s", packet.payload);
+ printf("B_input: sending ack and to layer 5: %s", packet.payload);
  pack.acknum = rec_seq;
  memcpy(pack.payload, packet.payload, 20);
  pack.checksum = calc_checksum(&packet);
@@ -181,9 +165,7 @@ void B_input(packet)
     printf("impossible");
  }
  //could be packet
-
 }
-
 /* the following routine will be called once (only) before any other */
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
