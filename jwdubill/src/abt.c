1cb58a055405632d84a5d9b60cc5f7135c554d96
joeydubill <32132614+joeydubill@users.noreply.github.com>
2021-12-01 14:00:48 -0500

    Update abt.c

--- jwdubill/src/abt.c
+++ jwdubill/src/abt.c
@@ -97,7 +97,7 @@ void A_input(packet)
  }else if (sender_seq == 1){
     sender_seq = 0;
  }else{
-    printf("impossible");
+    //printf("impossible");
  }
  wait_5 = 1;
  if (bufferreadindex < bufferwriteindex){
@@ -117,7 +117,7 @@ void A_input(packet)
 void A_timerinterrupt()
 {
  if (wait_5 != 0){
-    printf("A Timer interrupt, not waiting for ack, ignore");
+    //printf("A Timer interrupt, not waiting for ack, ignore");
     return;
  }
  starttimer(0, sender_inc);
@@ -148,7 +148,7 @@ void B_input(packet)
   struct pkt pack;
  //check checksum and seq
  if (packet.checksum != calc_checksum(&packet)){
-     printf("B_input: checksum error");
+     //printf("B_input: checksum error");
      /*
      pack.acknum = 1 - rec_seq;
      memmove(pack.payload, packet.payload, 20);
@@ -158,7 +158,7 @@ void B_input(packet)
      return;    
  }
  if (packet.seqnum != rec_seq) {
-     printf("B_input: seqnum not rec_seq");
+     //printf("B_input: seqnum not rec_seq");
       /*
      pack.acknum = 1 -rec_seq;
      memmove(pack.payload, packet.payload, 20);
@@ -179,7 +179,7 @@ void B_input(packet)
  }else if (rec_seq == 1){
     rec_seq = 0;
  }else{
-    printf("impossible");
+    //printf("impossible");
  }
  //could be packet
 
