5955c2dd2b1f00e86fcef4670ef0be550a2273ad
joeydubill <32132614+joeydubill@users.noreply.github.com>
2021-12-02 13:12:51 -0500

    Update gbn.c
    full

--- jwdubill/src/gbn.c
+++ jwdubill/src/gbn.c
@@ -15,30 +15,104 @@
 
 /********* STUDENTS WRITE THE NEXT SEVEN ROUTINES *********/
 
+float RTT = 25.0;
+int sender_seq; //A seq
+int sender_ack; //A ack
+int next_seq
+int b_seq: // what b expects
+int window_size;
+int base; 
+
+struct msg buffer[1000];
+int bufferwriteindex;
+int bufferreadindex;
+
+int calc_checksum(struct pkt *packet){
+  int sum = 0;
+  sum += packet->seqnum;
+  sum += packet->acknum;
+  for (int i =0; i < 20; i++){
+      sum += packet->payload[i];
+  }
+  return sum;
+}
+
+void send(){
+ //send in a loop 
+ while((next_seq < (base + window_size)) && (next_seq < bufferwriteindex)){
+  
+    struct msg message = buffer[bufferreadindex];
+    struct pkt packet;
+    memcpy(packet.payload, message.data, 20);
+    packet.seqnum = next_seq;
+    packet.acknum = sender_ack;
+    packet.checksum = calc_checksum(&packet);
+  
+    tolayer3(0, packet);
+    if (base = next_seq){
+       starttimer(0, RTT);
+    }
+    next_seq++;
+ }
+}
+
 /* called from layer 5, passed the data to be sent to other side */
 void A_output(message)
   struct msg message;
 {
-
+  //add message to queue
+  struct msg buff;
+  memcpy(buff.data, message.data, 20);
+  buffer[bufferwriteindex] = buff;
+  bufferwriteindex++;
+ 
+ //send data
+ send();
+ 
 }
 
 /* called from layer 3, when a packet arrives for layer 4 */
 void A_input(packet)
   struct pkt packet;
 {
-
+ 
+   //check if corrupt
+   bool corrupt = false;
+   if(packet.checksum == calc_checksum(&packet)){
+	  	  corrupt = false;
+   }else{
+      corrupt = true;
+   }
+ 
+   if(!corrupt){
+      base = packet.acknum + 1;
+      if (base == next_seq){
+        stoptimer(0);
+      }else{
+        stoptimer(0);
+        starttimer(0, RTT);
+      }
+   }
 }
 
 /* called when A's timer goes off */
 void A_timerinterrupt()
 {
-
+  next_seq = base;
+  send();
 }  
 
 /* the following routine will be called once (only) before any other */
 /* entity A routines are called. You can use it to do any initialization */
 void A_init()
 {
+ sender_seq = 0;
+ sender_ack = 0
+ window_size = getwinsize(); //software interface
+ next_seq = 0;
+ base = 0;
+ bufferwriteindex = 0;
+ bufferreadindex = 0;
 
 }
 
@@ -48,6 +122,29 @@ void A_init()
 void B_input(packet)
   struct pkt packet;
 {
+   //check if corrupt
+   bool corrupt = false;
+   if(packet.checksum == calc_checksum(&packet)){
+	  	  corrupt = false;
+   }else{
+      corrupt = true;
+   }
+ 
+   if(!corrupt && (b_seq == packet.seqnum)){
+      tolayer5(1, packet.payload);
+      //rack = receiving ack
+      struct pkt rack;
+      rack.acknum = b_seq;
+      rack.checksum = calc_checksum(&rack);
+      
+      tolayer3(1, rack);
+      b_seq++;
+    
+   }
+ 
+ 
+ 
+ 
 
 }
 
@@ -55,5 +152,6 @@ void B_input(packet)
 /* entity B routines are called. You can use it to do any initialization */
 void B_init()
 {
+ b_seq = 0; //expected seq
 
 }
